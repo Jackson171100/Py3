102544021e21bd7729d14f94be6d312454820e44
Jackson <soandso@icloud.com>
2026-07-17 09:53:40 -0400

    hi

--- Untitled-15.py
+++ Untitled-15.py
@@ -1,5 +1,12 @@
-class a1:
-    def __init__(self,a):
-                self.a=a 
-var1=a1("a")
-print(var1.a)
+import pyautogui
+from time import sleep
+pyautogui.keyDown("win")
+pyautogui.press("r")
+pyautogui.keyUp("win")
+pyautogui.write("firefox")
+pyautogui.press("enter")
+sleep(2)
+pyautogui.write("https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1")
+pyautogui.press("enter")
+sleep(2)
+pyautogui.press("space")
