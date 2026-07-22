7c383aa0894d1cfc3c7967060a54d9ffe8ba8ce6
Jackson <soandso@icloud.com>
2026-07-22 11:11:24 -0400

    hi

--- class binary_search_tree.py
+++ class binary_search_tree.py
@@ -1,3 +1,19 @@
 class Node:
+    def __init__(self,left,right,value):
+        self.left=left
+        self.right=right
+        self.value=value
+class binary_search_tree:
     def __init__(self):
-        pass
+        self.root=None
+    def append(self,value):
+        if self.root==None:
+            self.root=Node
+        else:
+            spcx=self.left
+            #intc=self.right 
+            while True:
+                if spcx==True:
+                    value-1
+                #if intc==True:
+                    #value+1
