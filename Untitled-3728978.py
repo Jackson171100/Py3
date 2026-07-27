52d028f11c1ffcd806ab81326a4b5346550d17ab
Jackson <soandso@icloud.com>
2026-07-27 13:30:05 -0400

    hi

--- Untitled-3728978.py
+++ Untitled-3728978.py
@@ -1,13 +1,21 @@
-class Board:
-    def __init__(self):
-        self.board=[["","",""]
-                    ["","",""]
-                    ["","",""]
-        ]
-    def potental_moves(self):
-        for row in range(3):
-            for col in range(3):
-                if(self.board[0][0]==""):
+
+def make_board():
+    board=[ ["","",""],
+            ["","",""],
+            ["","",""],
+    ]
+    return board
+def potental_moves(board):
+    for row in range(3):
+        for col in range(3):
+            if(board[row][col]==""):
+                list1=[]
+                list1.append((row,col))
+                return(list1)
+var1=make_board()
+print(var1)
+var2=potental_moves(var1)
+print(var2)
+ 
+          
             
-        
-                    
