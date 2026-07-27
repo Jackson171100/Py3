beff8ba593d1bc6d77ddbecbcf5fc7a4e3c1faca
Jackson <soandso@icloud.com>
2026-07-27 11:14:14 -0400

    HI

---
+++ Untitled-3728978.py
@@ -0,0 +1,13 @@
+class Board:
+    def __init__(self):
+        self.board=[["","",""]
+                    ["","",""]
+                    ["","",""]
+        ]
+    def potental_moves(self):
+        for row in range(3):
+            for col in range(3):
+                if(self.board[0][0]==""):
+            
+        
+                    
