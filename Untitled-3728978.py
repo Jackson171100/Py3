ac1a662a74726cfdcf525d064288dc3846041eb3
Jackson <soandso@icloud.com>
2026-07-29 09:54:13 -0400

    hay ya

--- Untitled-3728978.py
+++ Untitled-3728978.py
@@ -6,16 +6,22 @@ def make_board():
     ]
     return board
 def potental_moves(board):
+    list1=[]
     for row in range(3):
         for col in range(3):
             if(board[row][col]==""):
-                list1=[]
                 list1.append((row,col))
-                return(list1)
+    return(list1)
 var1=make_board()
 print(var1)
 var2=potental_moves(var1)
 print(var2)
- 
+def print_board(board):
+    for row in range(3):
+        for col in range(3):
+            print(board)
+
+
+print_board(var1)
           
             
--- stuff.py
+++ stuff.py
@@ -1,2 +1,53 @@
-for row in range(10):
-    print(row)
+# for row in range(10):
+#     print(row)
+
+def celsius_to_farenheight(num):
+    a = num* (9/5)
+    print("hi")
+    b = a +32
+    return b
+example=[ [1,2,3],
+        [10,11,12],
+        [13,14,15],
+]   
+
+# print(example[0][0])
+# print(example[1][0])
+# print(example[2][1])
+# print(example[1][2])
+# print(example[0][1])
+# print(example[2][2])
+# for row in range(3):
+#     for col in range(3):
+#         print(row,col)
+
+# for row in range(3):
+#     print(row)
+#     for col in range(3):
+#         print(col)
+
+# for row in range(2):
+#     print(row)
+#     for col in range(2):
+#         print(col)
+
+for row in range(3):
+    for col in range(3):
+        print(row, col)
+"""
+0 0
+
+"""
+
+# for row in range(3):
+#     for col in range(3):
+#         print(example[row][col])
+
+# board=[ ["","",""],
+#         ["","",""],
+#         ["","",""],
+# ]   
+# print(celsius_to_farenheight(23423))
+# print(celsius_to_farenheight(2123423))
+# print(celsius_to_farenheight(234223))
+# print(celsius_to_farenheight(231423))
